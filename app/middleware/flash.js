6b3a5ff0e8f0eeb2b28989839c8c7cb63d99eafd
jackliu <jackliulovelt@outlook.com>
2018-10-01 19:22:59 +0800

    :tada: init

---
+++ app/middleware/flash.js
@@ -0,0 +1,32 @@
+/* istanbul ignore file */
+module.exports = ({ key }, app) => async (ctx, next) => {
+  ctx.session[key] = ctx.session[key] || {}
+  const flash = ctx.session[key]
+  ctx.session[key] = {}
+  
+  function set(msg) {
+    ctx.session[key] = msg
+  }
+
+  const get = () => flash
+
+  Object.defineProperty(ctx, 'flash', {
+    set,
+    get,
+    enumerable: true
+  })
+
+  ctx.request.flash = (type, msg) => {
+    ctx.flash = { type, message: msg }
+  }
+
+  ['success', 'warning', 'error', 'info'].forEach(type => {
+    ctx['flash_' + type] = msg => (ctx.flash = { type, message: msg})
+  })
+
+  await next()
+
+  if (ctx.status === 302 && ctx.session && !ctx.session[key]) {
+    ctx.session[key] = flash
+  }
+}
---
+++ config/config.default.js
@@ -0,0 +1,11 @@
+'use strict';
+
+/**
+ * egg-msg-flash default config
+ * @member Config#msgFlash
+ * @property {String} SOME_KEY - some description
+ */
+exports.flash = {
+  key: 'flash',
+};
+
